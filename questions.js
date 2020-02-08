7b1a7cf76cb1a9579d6f5948e9016073702d10ae
Mathew <mjstthomas0516@gmail.com>
2020-02-08 10:52:48 -0700

    added questions object

---
+++ questions.js
@@ -0,0 +1,62 @@
+const quiz = [
+{
+	id: 1,
+	question: "What are the only two countries that DO NOT sell Coca-cola?",
+	answers: ["Argentina and Brazil", "Holland and Cuba", "Cuba and North Korea", "Russia and Iran"],
+	correct: "Cuba and North Korea"
+},
+{
+	id: 2,
+	question: "The entire human populatin can fit into which city?",
+	answers: ["Los Angeles", "New York City", "Paris", "Tokyo"],
+	correct: "Los Angeles"
+},
+{
+	id: 3,
+	question: "Where can the most visited country be found?",
+	answers: ["United States", "Italy", "France", "China"],
+	correct: "France"
+},
+{
+	id: 4,
+	question: "How many countries DO NOT use the metric system?",
+	answers: ["2", "8", "3", "1"],
+	correct: "3"
+},
+{
+	id: 5,
+	question: "What is the youngest country?",
+	answers: ["Moraco", "United States", "South Sudan", "Taiwan"],
+	correct: "South Sudan",
+},
+{
+	id: 6,
+	question: "What is the most spoken language on earth?",
+	answers: ["English", "French", "Spanish", "Mandarin"]
+	correct: "Mandarin"
+},
+{
+	id: 7,
+	question: "Which city is the most 'bike friendly'?",
+	answers: ["New York City", "Toronto", "London", "Copenhagen"]
+	correct: "Copenhagen",
+},
+{
+	id: 8,
+	question: "Facebook has more users that the total population of which 3 countries?",
+	answers: ["U.S., China, and Brazil", "Puerto Rico, Cuba, and Mexico", "Argentina, Brazil, and Venezuela", "South Africa, Congo, and Uruguay"]
+	correct: "U.S., China, and Brazil",
+},
+{
+	id: 9,
+	question: "Which country has more pets per household?",
+	answers: ["Austrailia", "Greece", "Turkey", "New Zealand"]
+	correct: "New Zealand",
+},
+{
+	id: 10,
+	question: "Which is the World's largest city?",
+	answers: ["Moscow", "Rome", "Los Angeles", "Tokyo"]
+	correct: "Tokyo",
+}
+]
